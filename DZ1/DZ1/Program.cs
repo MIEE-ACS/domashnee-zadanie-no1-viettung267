ec3a49a4fb57932671eac20d05edd837c2bcd08f
viettung267 <70976064+viettung267@users.noreply.github.com>
2020-09-14 23:22:27 +0300

    Домашнее задание №1_ХоангВьетТунг_УТС-22

---
+++ DZ1/DZ1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Dz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number in radians : ");
+            float beforeNumber = float.Parse(Console.ReadLine());
+            while ((beforeNumber <= 0) || (beforeNumber >= 2))
+            {
+                Console.WriteLine("Please enter number in the range 0 <a <2 : ");
+                beforeNumber = float.Parse(Console.ReadLine());
+            }
+            float afterNumber = beforeNumber * 180;
+            Console.Write($"Number in degrees : {afterNumber}");
+        }
+    }
+}
